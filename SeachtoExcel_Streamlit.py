4e54b0a955a1dff93107523db9af5fd509eda818
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-12 17:41:13 +0100

    Update SeachtoExcel_Streamlit.py

--- SeachtoExcel_Streamlit.py
+++ SeachtoExcel_Streamlit.py
@@ -1,12 +1,27 @@
 import os
-import re
+import xlsxwriter
 import streamlit as st
-import pandas as pd
 
+# Set up the Streamlit app
+st.title("File Search to Excel")
+st.write("Enter search criteria and select folder to search in.")
 
-def search_files(search_name, folder_path, output_file_path):
-    # Create an empty DataFrame to store the search results
-    results_df = pd.DataFrame(columns=["Search Name", "File Path"])
+# Get user input for search name and folder path
+search_name = st.text_input("Enter file name to search for:")
+folder_path = st.file_input("Enter folder path to search in:", type="directory")
+output_file_path = st.file_uploader("Select output file path:", type=["xls", "xlsx"])
+
+# If the user has selected a valid output file path, create a new Excel workbook and worksheet
+if output_file_path is not None:
+    workbook = xlsxwriter.Workbook(output_file_path)
+    worksheet = workbook.add_worksheet()
+
+    # Set up column headers in the worksheet
+    worksheet.write(0, 0, "Search Name")
+    worksheet.write(0, 1, "File Path")
+
+    # Initialize row counter
+    row = 1
 
     # Set up search criteria
     search_criteria = ".*{}.*".format(search_name)
@@ -15,25 +30,13 @@ def search_files(search_name, folder_path, output_file_path):
     with os.scandir(folder_path) as entries:
         for entry in entries:
             if entry.is_file() and re.search(search_criteria, entry.name):
-                # Add the search name and file path to the results DataFrame
-                results_df = results_df.append(
-                    {"Search Name": search_name, "File Path": entry.path}, ignore_index=True
-                )
+                # Write the search name and file path to the worksheet
+                worksheet.write(row, 0, search_name)
+                worksheet.write(row, 1, entry.path)
+                row += 1
 
-    # Write the results to the output Excel file
-    results_df.to_excel(output_file_path, index=False)
+    # Close the Excel workbook
+    workbook.close()
 
     # Print a message to indicate that the search is complete
-    st.success(f"Search complete. Results saved to {output_file_path}")
-
-
-# Set up the Streamlit app
-st.title("Search for Files")
-search_name = st.text_input("Enter file name to search for:")
-folder_path = st.file_input("Enter folder path to search in:", type="directory")
-output_file_path = st.file_uploader("Select output Excel file:", type=["xlsx"])
-
-# Wait for the user to select an output file
-if output_file_path is not None:
-    # Run the search function with the user inputs
-    search_files(search_name, folder_path, output_file_path.name)
+    st.write("Search complete. Results saved to {}.".format(output_file_path))
