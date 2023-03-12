4cb9edcdaf8b85825a0c147337a8908a2ec98107
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-12 16:55:35 +0100

    Update SeachtoExcel_Streamlit.py

--- SeachtoExcel_Streamlit.py
+++ SeachtoExcel_Streamlit.py
@@ -1,18 +1,11 @@
 import os
 import xlsxwriter
-import tkinter as tk
-from tkinter import filedialog
-import time
+import streamlit as st
 
 # Get user input for search name and folder path
-search_name = input("Enter file name to search for: ")
-
-# Create a Tkinter window to browse for a folder
-root = tk.Tk()
-root.withdraw()
-
-folder_path = filedialog.askdirectory(title="Select Folder to Search In")
-output_file_path = input("Enter output file path: ")
+search_name = st.text_input("Enter file name to search for: ")
+folder_path = st.file_input("Enter folder path to search in: ", type="directory")
+output_file_path = st.file_uploader("Enter output file path: ", type=["xlsx"])
 
 # Create a new Excel workbook and worksheet
 workbook = xlsxwriter.Workbook(output_file_path)
@@ -28,9 +21,6 @@ row = 1
 # Set up search criteria
 search_criteria = ".*{}.*".format(search_name)
 
-# Start timer
-start_time = time.time()
-
 # Search for files in the folder path that match the search name using os.scandir()
 with os.scandir(folder_path) as entries:
     for entry in entries:
@@ -43,8 +33,5 @@ with os.scandir(folder_path) as entries:
 # Close the Excel workbook
 workbook.close()
 
-# Calculate elapsed time
-elapsed_time = time.time() - start_time
-
 # Print a message to indicate that the search is complete
-print("Search complete. Results saved to {}. Elapsed time: {:.2f} seconds".format(output_file_path, elapsed_time))
+st.success("Search complete. Results saved to {}. Elapsed time: {:.2f} seconds".format(output_file_path, elapsed_time))
