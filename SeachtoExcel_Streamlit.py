00452eed6080704b384f023e46706338787225dd
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2023-03-12 16:59:42 +0100

    Update SeachtoExcel_Streamlit.py

--- SeachtoExcel_Streamlit.py
+++ SeachtoExcel_Streamlit.py
@@ -1,58 +1,46 @@
 import os
-import re
 import xlsxwriter
+import time
 import streamlit as st
 
 # Get user input for search name and folder path
-search_name = st.text_input("Enter file name to search for: ")
-folder_path = st.file_input("Enter folder path to search in: ", type="directory")
-output_file_path = st.text_input("Enter output file path: ")
+search_name = st.text_input("Enter file name to search for:")
+output_file_path = st.file_uploader("Select output Excel file:", type=["xlsx", "xls"])
 
 # Create a new Excel workbook and worksheet
-workbook = xlsxwriter.Workbook(output_file_path)
-worksheet = workbook.add_worksheet()
+if output_file_path is not None:
+    workbook = xlsxwriter.Workbook(output_file_path)
+    worksheet = workbook.add_worksheet()
 
-# Set up column headers in the worksheet
-worksheet.write(0, 0, "Search Name")
-worksheet.write(0, 1, "File Path")
+    # Set up column headers in the worksheet
+    worksheet.write(0, 0, "Search Name")
+    worksheet.write(0, 1, "File Path")
 
-# Initialize row counter
-row = 1
+    # Initialize row counter
+    row = 1
 
-# Set up search criteria
-search_criteria = ".*{}.*".format(search_name)
+    # Set up search criteria
+    search_criteria = ".*{}.*".format(search_name)
 
-# Search for files in the folder path that match the search name using os.scandir()
-with os.scandir(folder_path) as entries:
-    for entry in entries:
-        if entry.is_file() and re.search(search_criteria, entry.name):
-            # Write the search name and file path to the worksheet
-            worksheet.write(row, 0, search_name)
-            worksheet.write(row, 1, entry.path)
-            row += 1
+    # Start timer
+    start_time = time.time()
 
-# Close the Excel workbook
-workbook.close()
+    # Search for files in the folder path that match the search name using os.scandir()
+    folder_path = st.file_input("Enter folder path to search in:", type="directory")
+    if folder_path is not None:
+        with os.scandir(folder_path) as entries:
+            for entry in entries:
+                if entry.is_file() and re.search(search_criteria, entry.name):
+                    # Write the search name and file path to the worksheet
+                    worksheet.write(row, 0, search_name)
+                    worksheet.write(row, 1, entry.path)
+                    row += 1
 
-# Print a message to indicate that the search is complete
-st.write("Search complete. Results saved to {}".format(output_file_path))
+        # Close the Excel workbook
+        workbook.close()
 
+        # Calculate elapsed time
+        elapsed_time = time.time() - start_time
 
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Read the Excel file into a pandas dataframe
-df = pd.read_excel(output_file_path)
-
-# Group the dataframe by the search name and count the number of files for each name
-grouped_df = df.groupby("Search Name").count()
-
-# Create a bar plot of the grouped data
-fig, ax = plt.subplots()
-ax.bar(grouped_df.index, grouped_df["File Path"])
-ax.set_xlabel("Search Name")
-ax.set_ylabel("Number of Files")
-ax.set_title("Files Found by Search Name")
-
-# Show the plot
-st.pyplot(fig)
+        # Print a message to indicate that the search is complete
+        st.write("Search complete. Results saved to {}. Elapsed time: {:.2f} seconds".format(output_file_path.name, elapsed_time))
